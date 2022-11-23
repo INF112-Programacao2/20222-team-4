2089845913a199e7e59dff705530746892f90809
Matheus515 <cvslisboa@hotmail.com>
2022-11-23 09:45:01 -0300

    editando Estoque .h

--- estoque/estoque.h
+++ estoque/estoque.h
@@ -4,6 +4,12 @@
 
 class Estoque {
     private: 
+
+    public:
+        void adcionarProduto(int id, std::string nome, int quantidadeProduto, double valor_Produto);
+        void removerProduto(int id, std::string nome, int quantidadeProduto);
+        bool consultarEstoque(int id, std::string nome, int quantidadeProduto);
+
         
     
 };
